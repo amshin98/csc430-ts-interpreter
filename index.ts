e1afd744a29980f7a1660951594425bc9229218d
nguyen-darianhuy <17971850+nguyen-darianhuy@users.noreply.github.com>
2020-03-11 20:22:11 -0700

    Interp: Allow using an env in interp

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 class NumC {
-    type = "NumC" as const
-    constructor(public n: number) {}
-  }
+   type = 'NumC' as const;
+   constructor(public n: number) {}
+}
 // class StrC {
 //     type = "StrC" as const
 //     constructor(public str: string) {}
@@ -11,87 +11,93 @@ class NumC {
 //     constructor(public bool: Boolean) {}
 // }
 class IdC {
-    type = "IdC" as const
-    constructor(public s: number) {}
+   type = 'IdC' as const;
+   constructor(public s: string) {}
 }
 // class IfC {
 //     type = "IfC" as const
 //     constructor(public tst: ExprC, public thn: ExprC, public els: ExprC) {}
 // }
 // class AppC {
-//     type = "AppC" as const
-//     constructor(public func: ExprC, public arg: ExprC[]) {}
+//    type = 'AppC' as const;
+//    constructor(public func: ExprC, public arg: ExprC[]) {}
 // }
 // class LamC {
 //     type = "LamC" as const
 //     constructor(public params: symbol[], public body: ExprC) {}
 // }
+type ExprC = NumC | IdC //| AppC; //| StrC | BoolC | IfC | LamC
 
 // Environment
 interface HashTable<T> {
-  [key : string] : T;
+   [key: string]: T;
 }
 
 // Values
 class NumV {
-    type = "NumV" as const
-    constructor(public n: number) {}
+   type = 'NumV' as const;
+   constructor(public n: number) {}
 }
 class StrV {
-    type = "StrV" as const
-    constructor(public str: string) {}
+   type = 'StrV' as const;
+   constructor(public str: string) {}
 }
 class BoolV {
-    type = "BoolV" as const
-    constructor(public bool: boolean) {}
+   type = 'BoolV' as const;
+   constructor(public bool: boolean) {}
 }
 class CloV {
-    type = "CloV" as const
-    constructor(public params: string[], public body: ExprC, public env: Map<string, Value>) {}
+   type = 'CloV' as const;
+   constructor(
+      public params: string[],
+      public body: ExprC,
+      public env: Map<string, Value>,
+   ) {}
 }
 class PrimV {
-    type = "PrimV" as const
-    constructor(public op: any) {}
+   type = 'PrimV' as const;
+   constructor(public op: any) {}
 }
-type Value = NumV | StrV | BoolV | CloV | PrimV
+type Value = NumV | StrV | BoolV | CloV | PrimV;
 
 // Environment
-function init_mt_env() {
-  var env = new Map();
-  env.set("+", new PrimV(my_add));
-  return env;
+function init_mt_env(): Map<string, Value> {
+   var env = new Map<string, Value>();
+   env.set('null', new StrV('null'));
+   env.set('+', new PrimV(my_add));
+   return env;
 }
 
-function my_add(left : Value, right : Value) {
-  return (<NumV> left).n + (<NumV> right).n;
+function my_add(left: Value, right: Value) {
+   return (<NumV>left).n + (<NumV>right).n;
 }
 
 // extends the environment
-function extend_env(new_env : Map<string, Value>, old_env : Map<string, Value>) {
-  return new Map([...old_env, ...new_env]);
+function extend_env(new_env: Map<string, Value>, old_env: Map<string, Value>) {
+   return new Map([...old_env, ...new_env]);
 }
 
-type ExprC = NumC | IdC//| StrC | BoolC | IdC | IfC | AppC | LamC
-type ExprCType = ExprC["type"]
+/*
+ * Visitor TypeScript implementation of Racket's pattern matching
+ * inspired by https://gist.github.com/pufface/6a2050b21a692d2400c35bbe3536552c
+ */
+type ExprCType = ExprC['type'];
 
-type ExprCMap<U> = { [K in ExprCType]: U extends { type: K } ? U : never }
-type ExprCTypeMap = ExprCMap<ExprC>
+type ExprCMap<U> = { [K in ExprCType]: U extends { type: K } ? U : never };
+type ExprCTypeMap = ExprCMap<ExprC>;
 
-type Pattern<T> = { [K in keyof ExprCTypeMap]: (expr: ExprCTypeMap[K]) => T }
+type Pattern<T> = { [K in keyof ExprCTypeMap]: (expr: ExprCTypeMap[K]) => T };
 
-function match<T>(pattern: Pattern<T>, env: Map<string, Value>): (expr: ExprC) => T {
-  // https://github.com/Microsoft/TypeScript/issues/14107
-  return expr => pattern[expr.type](expr as any)
+function interp(env: Map<string, Value>): (expr: ExprC) => Value {
+   const pattern: Pattern<Value> = {
+      NumC: ({ n }) => new NumV(n),
+      IdC: ({ s }) => <Value>env.get(s),
+   };
+
+   return expr => pattern[expr.type](expr as any);
 }
 
-const exprs = [new NumC(3), new NumC(5)]
+const topInterp = interp(init_mt_env());
 
-const interp = match<number>({
-  NumC: ({n}) => n,
-  IdC: ({s}) => s,
-}, init_mt_env())
-
-console.log(interp(new IdC(123)));
-
-export { NumC, IdC };
-export default interp;
+export { NumC, IdC, Value, NumV, StrV };
+export default topInterp;
--- test.ts
+++ test.ts
@@ -1,8 +1,12 @@
-import interp, { NumC } from './index';
+import interp, { IdC, NumC, Value, NumV, StrV } from './index';
 
 describe('1-depth exprC tests', function() {
    it('NumC', function() {
-      const result: number = interp(new NumC(123));
-      expect(result).toBe(123);
+      const result: Value = interp(new NumC(123));
+      expect(result).toEqual(new NumV(123));
+   });
+   it('IdC', function() {
+      const result: Value = interp(new IdC('null'));
+      expect(result).toEqual(new StrV('null'));
    });
 });
